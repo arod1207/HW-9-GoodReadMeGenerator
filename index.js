405bb4f87ff78b89d9ccdf5e6d5494c7c3ca2845
Arod1207 <arod1207@me.com>
2020-06-14 18:51:10 -0500

    username  and user image search working

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
-const axios = require("axios");
+const axios = require('axios');
+const inquirer = require('inquirer')
 
-let userName = "arod1207"
+let userName = "NijTeck"
 
 axios
   .get(`https://api.github.com/users/${userName}/repos?per_page=100`)
   .then(function (response) {
     // handle success
     console.log(response);
+    console.log("Retrieved user data")
+    let user = response.data[0].owner.login;
+    let picture = response.data[0].owner.avatar_url;
+
+    console.log(user);
+    console.log(picture);
+    
+
+   
+   
   })
   .catch(function (error) {
     // handle error
     console.log(error);
+    console.log("There was an error retrieving user data")
   })
   .finally(function () {
     // always executed
   });
+
+  
