d8517d7370a24658f7579765311536eac0ea6905
Arod1207 <arod1207@me.com>
2020-06-14 19:38:49 -0500

    info being pulled in with axios through inqurier

--- index.js
+++ index.js
@@ -1,9 +1,35 @@
 const axios = require('axios');
 const inquirer = require('inquirer')
 
-let userName = "NijTeck"
 
-axios
+
+// asking users questions //
+
+
+function promptUser() {
+  return inquirer.prompt([
+    {
+      type: "input",
+      message: "What is your GitHub username?",
+      name: "userName",
+    }
+  ]);
+}
+// saved the function as a variable
+user = promptUser();
+user.then(function(user){
+  getUserData(user.userName);
+
+  let readme = ``
+}).catch(function(err){
+  console.log(err);
+})
+  
+
+// getting information returned from github
+
+function getUserData(userName) {
+  axios
   .get(`https://api.github.com/users/${userName}/repos?per_page=100`)
   .then(function (response) {
     // handle success
@@ -11,12 +37,7 @@ axios
     console.log("Retrieved user data")
     let user = response.data[0].owner.login;
     let picture = response.data[0].owner.avatar_url;
-
-    console.log(user);
-    console.log(picture);
     
-
-   
    
   })
   .catch(function (error) {
@@ -27,5 +48,8 @@ axios
   .finally(function () {
     // always executed
   });
+}
+
+
 
   
