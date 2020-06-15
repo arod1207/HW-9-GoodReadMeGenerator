d59e2c2a9a42c3acb6e2188c2eed3887798fc30e
Arod1207 <arod1207@me.com>
2020-06-15 01:05:19 -0500

    Option to insert profile image

--- README.md
+++ README.md
@@ -1,22 +1,22 @@
-# Is it really working
+# sleepy
 
-  ## Oh crap it might be
+  ## tired
   
-  ## lots of things to take care of here
+  ## sleepy
   
-  * Very easy
-  * Do what you want
-  * BSD
-  * Everyone
-  
-  
-  ## Very easy
+  * tire
+  * sleepy tired
+  * Apache
+  * apache
   
   
-  ## Do what you want
+  ## tire
   
   
-  ## BSD
+  ## sleepy tired
+  
+  
+  ## Apache
   
   
   🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
@@ -28,11 +28,13 @@
   Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
   
   
-  ## Everyone
+  ## apache
   
   If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
   
-  ## no test
+  ## 
   
   Go the extra mile and write tests for your application. Then provide examples on how to run them.
+
+  ![Users GitHub Profile Image](no)
   
--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-const fs = require('fs');
-const axios = require('axios');
-const inquirer = require('inquirer')
-
-
+const fs = require("fs");
+const axios = require("axios");
+const inquirer = require("inquirer");
 
 // asking users questions //
-
 
 function promptUser() {
   return inquirer.prompt([
@@ -42,9 +39,9 @@ function promptUser() {
     {
       type: "list",
       message: "License: ",
-      choices: ['Apache', 'BSD', 'GNU', 'MIT'],
+      choices: ["Apache", "BSD", "GNU", "MIT"],
       name: "license",
-     },
+    },
     {
       type: "input",
       message: "Contributing: ",
@@ -54,38 +51,50 @@ function promptUser() {
       type: "input",
       message: "Test: ",
       name: "test",
-    }
+    },
+    {
+      type: "list",
+      message: "Would you like to add your picture?: ",
+      choices: ["Yes", "No"],
+      name: "userPic",
+      filter: function (val) {
+        return val.toLowerCase();
+      },
+    },
+    {
+      type: "list",
+      message: "Would you like to add your email address?: ",
+      choices: ["Yes", "No"],
+      name: "userEmail",
+      filter: function (val) {
+        return val.toLowerCase();
+      },
+    },
   ]);
 }
 // saved the function as a variable
 user = promptUser();
-user.then(function(user){
+user.then(function (user) {
   getUserData(user.userName);
 
   function getUserData(userName) {
     axios
-    .get(`https://api.github.com/users/${userName}/repos?per_page=100`)
-    .then(function (response) {
-      // handle success
-      // console.log(response); // comment out at the end
-      console.log("Retrieved user data")
-      let user = response.data[0].owner.login;
-      let picture = response.data[0].owner.avatar_url;
-      
-     
-    })
-    .catch(function (error) {
-      // handle error
-      console.log(error);
-      console.log("There was an error retrieving user data")
-    })
-    .finally(function () {
-      // always executed
-    });
-  }
-  
+      .get(`https://api.github.com/users/${userName}/repos?per_page=100`)
+      .then(function (response) {
+        // handle success
+        // console.log(response); // comment out at the end
+        console.log("Retrieved user data");
 
-  let readme = `# ${user.projectTitle}
+        let picture = response.data[0].owner.avatar_url;
+
+        if (user.userPic === "yes") {
+          user.userPic = picture;
+          console.log(user.userPic);
+        } else {
+          console.log("no picture")
+        }
+
+        let readme = `# ${user.projectTitle}
 
   ## ${user.projectDesc}
   
@@ -122,24 +131,81 @@ user.then(function(user){
   ## ${user.test}
   
   Go the extra mile and write tests for your application. Then provide examples on how to run them.
-  `
 
-  // end of generated Readme.md
+  ![Users GitHub Profile Image](${user.userPic})
+  `;
 
-  // create readme file
+        // end of generated Readme.md
 
-  fs.writeFile('README.md', readme, function(err){
-    if (err) throw err;
-    console.log('README.md Created')
-  })
-}).catch(function(err){
-  console.log(err);
-})
-  
+        // create readme file
+
+        fs.writeFile("README.md", readme, function (err) {
+          if (err) throw err;
+          console.log("README.md Created");
+        });
+      })
+      .catch(function (err) {
+        console.log("File was not created")
+        console.log(err);
+      })
+
+      .catch(function (error) {
+        // handle error
+        console.log(error);
+        console.log("There was an error retrieving user data");
+      })
+      // .finally(function () {
+      //   // always executed
+      // });
+  }
+});
+
+//   let readme = `# ${user.projectTitle}
+
+//   ## ${user.projectDesc}
+
+//   ## ${user.tableOfCont}
+
+//   * ${user.install}
+//   * ${user.usage}
+//   * ${user.license}
+//   * ${user.contributing}
+
+//   ## ${user.install}
+
+//   ## ${user.usage}
+
+//   ## ${user.license}
+
+//   🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+
+//   ## Badges
+
+//   ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+
+//   Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+
+//   ## ${user.contributing}
+
+//   If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+
+//   ## ${user.test}
+
+//   Go the extra mile and write tests for your application. Then provide examples on how to run them.
+
+//   ![Users GitHub Profile Image](${user.userPic})
+//   `
+
+//   // end of generated Readme.md
+
+//   // create readme file
+
+//   fs.writeFile('README.md', readme, function(err){
+//     if (err) throw err;
+//     console.log('README.md Created')
+//   })
+// }).catch(function(err){
+//   console.log(err);
+// })
 
 // getting information returned from github
-
-
-
-
-  
