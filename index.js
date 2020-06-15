cdbedeaa9f5cec4c653b79c80ec1adec94c9383f
Arod1207 <arod1207@me.com>
2020-06-14 20:28:26 -0500

    created readme with fs

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 const axios = require('axios');
 const inquirer = require('inquirer')
 
@@ -10,8 +11,49 @@ function promptUser() {
   return inquirer.prompt([
     {
       type: "input",
-      message: "What is your GitHub username?",
+      message: "What is your GitHub username?: ",
       name: "userName",
+    },
+    {
+      type: "input",
+      message: "Project Title: ",
+      name: "projectTitle",
+    },
+    {
+      type: "input",
+      message: "Project Description: ",
+      name: "projectDesc",
+    },
+    {
+      type: "input",
+      message: "Table of Contents: ",
+      name: "tableOfCont",
+    },
+    {
+      type: "input",
+      message: "Installation: ",
+      name: "install",
+    },
+    {
+      type: "input",
+      message: "Usage: ",
+      name: "usage",
+    },
+    {
+      type: "list",
+      message: "License: ",
+      choices: ['Apache', 'BSD', 'GNU', 'MIT'],
+      name: "license",
+     },
+    {
+      type: "input",
+      message: "Contributing: ",
+      name: "contributing",
+    },
+    {
+      type: "input",
+      message: "Test: ",
+      name: "test",
     }
   ]);
 }
@@ -20,7 +62,76 @@ user = promptUser();
 user.then(function(user){
   getUserData(user.userName);
 
-  let readme = ``
+  function getUserData(userName) {
+    axios
+    .get(`https://api.github.com/users/${userName}/repos?per_page=100`)
+    .then(function (response) {
+      // handle success
+      // console.log(response); // comment out at the end
+      console.log("Retrieved user data")
+      let user = response.data[0].owner.login;
+      let picture = response.data[0].owner.avatar_url;
+      
+     
+    })
+    .catch(function (error) {
+      // handle error
+      console.log(error);
+      console.log("There was an error retrieving user data")
+    })
+    .finally(function () {
+      // always executed
+    });
+  }
+  
+
+  let readme = `# ${user.projectTitle}
+
+  ## ${user.projectDesc}
+  
+  ## ${user.tableOfCont}
+  
+  * ${user.install}
+  * ${user.usage}
+  * ${user.license}
+  * ${user.contributing}
+  
+  
+  ## ${user.install}
+  
+  
+  ## ${user.usage}
+  
+  
+  ## ${user.license}
+  
+  
+  🏆 The sections listed above are the minimum for a good README, but your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+  
+  ## Badges
+  
+  ![badmath](https://img.shields.io/github/languages/top/nielsenjared/badmath)
+  
+  Badges aren't _necessary_, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+  
+  
+  ## ${user.contributing}
+  
+  If you created an application or package and would like other developers to contribute it, you will want to add guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own.
+  
+  ## ${user.test}
+  
+  Go the extra mile and write tests for your application. Then provide examples on how to run them.
+  `
+
+  // end of generated Readme.md
+
+  // create readme file
+
+  fs.writeFile('README.md', readme, function(err){
+    if (err) throw err;
+    console.log('README.md Created')
+  })
 }).catch(function(err){
   console.log(err);
 })
@@ -28,27 +139,6 @@ user.then(function(user){
 
 // getting information returned from github
 
-function getUserData(userName) {
-  axios
-  .get(`https://api.github.com/users/${userName}/repos?per_page=100`)
-  .then(function (response) {
-    // handle success
-    console.log(response);
-    console.log("Retrieved user data")
-    let user = response.data[0].owner.login;
-    let picture = response.data[0].owner.avatar_url;
-    
-   
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-    console.log("There was an error retrieving user data")
-  })
-  .finally(function () {
-    // always executed
-  });
-}
 
 
 
