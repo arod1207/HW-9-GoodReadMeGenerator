029b51dfafb37aa32c0d688aeb5c8455b3bc18e5
Arod1207 <arod1207@me.com>
2020-06-17 15:46:39 -0500

    add user generated headings

--- README.md
+++ README.md
@@ -1,29 +1,36 @@
 
 
-#    
+# good read me   
     
-## 
+## Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
+    
+* [Installation](#installation)
+* [Usage](#usage)
+* [Test](#test)
+* [Contributions](#contributions)
+     
+## Installation
+ ### Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s  
       
- 
- 
- 
- 
+## Usage
+ ### Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
       
-### 
-      
-### 
-      
- 
-      
-![Top Language](https://img.shields.io/github/languages/top/arod1207/) ![GitHub last commit](https://img.shields.io/github/last-commit/arod1207/)  ![GitHub Followers](https://img.shields.io/github/followers/arod1207?style=social)
+## License
+ [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+## Test
+ ### Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
         
-### 
+## Contributions
+ ### Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s
       
       
-### 
+
+
+![Top Language](https://img.shields.io/github/languages/top/arod1207/hw-9-goodreadmegenerator) ![GitHub last commit](https://img.shields.io/github/last-commit/arod1207/hw-9-goodreadmegenerator)  ![GitHub Followers](https://img.shields.io/github/followers/arod1207?style=social)
       
 ![Users GitHub Profile Image](https://avatars3.githubusercontent.com/u/18537011?v=4)   
 
-#### 
+#### arod1207@me.com
     
       
--- index.js
+++ index.js
@@ -31,7 +31,7 @@ inquirer
     },
     {
       type: "checkbox",
-      message: "Table of Contents Options: ",
+      message: "Table of Contents?: ",
       name: "tableOfCont",
       choices: [
         {
@@ -41,10 +41,10 @@ inquirer
           name: "Usage",
         },
         {
-          name: "Credits",
+          name: "Test",
         },
         {
-          name: "License",
+          name: "Contributions",
         },
       ],
     },
@@ -100,7 +100,7 @@ inquirer
       .then(function (response) {
         // handle success
         // console.log(response);
-        console.log(answers.license)
+       
 
         let picture = response.data.avatar_url;
 
@@ -116,15 +116,15 @@ inquirer
           answers.license = `### ${answers.license}`;
         }
 
-        // if statements for licesne selection
+        // if statements for license selection
         if (answers.license === "Apache"){
-          answers.license = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
+          answers.license = `## License\n [![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`
         } else if (answers.license === "BSD") {
-          answers.license = `[![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
+          answers.license = `## License\n [![License](https://img.shields.io/badge/License-BSD%203--Clause-blue.svg)](https://opensource.org/licenses/BSD-3-Clause)`
         } else if (answers.license === "GNU"){
-          answers.license = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
+          answers.license = `## License\n [![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`
         } else if (answers.license === "MIT") {
-          answers.license = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
+          answers.license = `## License\n [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`
         } else if (answers.license === "NONE") {
           answers.license = " ";
         } else {
@@ -144,50 +144,80 @@ inquirer
         if (answers.tableOfCont[0] === undefined) {
           answers.tableOfCont[0] = " ";
         } else {
-          answers.tableOfCont[0] = `* ${answers.tableOfCont[0]}`;
+          answers.tableOfCont[0] = `* [${answers.tableOfCont[0]}](#installation)`;
         }
 
         if (answers.tableOfCont[1] === undefined) {
           answers.tableOfCont[1] = " ";
         } else {
-          answers.tableOfCont[1] = `* ${answers.tableOfCont[1]}`;
+          answers.tableOfCont[1] = `* [${answers.tableOfCont[1]}](#usage)`;
         }
 
         if (answers.tableOfCont[2] === undefined) {
           answers.tableOfCont[2] = " ";
         } else {
-          answers.tableOfCont[2] = `* ${answers.tableOfCont[2]}`;
+          answers.tableOfCont[2] = `* [${answers.tableOfCont[2]}](#test)`;
         }
 
         if (answers.tableOfCont[3] === undefined) {
           answers.tableOfCont[3] = " ";
         } else {
-          answers.tableOfCont[3] = `* ${answers.tableOfCont[3]}`;
+          answers.tableOfCont[3] = `* [${answers.tableOfCont[3]}](#contributions)`;
         }
+
+        // if statements for headings //
+
+        if (answers.install === ' '){
+          answers.install = ' ';
+        } else if (answers.install = answers.install){
+          answers.install = `## Installation\n ### ${answers.install}`
+        }
+
+        if (answers.usage === ' '){
+          answers.usage = ' ';
+        } else if (answers.usage = answers.usage){
+          answers.usage = `## Usage\n ### ${answers.usage}`
+        }
+
+        if (answers.contributing === ' '){
+          answers.contributing = ' ';
+        } else if (answers.contributing = answers.contributing){
+          answers.contributing = `## Contributions\n ### ${answers.contributing}`
+        }
+
+        if (answers.test === ' '){
+          answers.test = ' ';
+        } else if (answers.test = answers.test){
+          answers.test = `## Test\n ### ${answers.test}`
+        }
+
+
 //  Start of ReadME markdown //
         let readme = `
 
 # ${answers.projectTitle}   
     
 ## ${answers.projectDesc}
-      
+    
 ${answers.tableOfCont[0]}
 ${answers.tableOfCont[1]}
 ${answers.tableOfCont[2]}
 ${answers.tableOfCont[3]}
+     
+${answers.install}  
       
-### ${answers.install}
-      
-### ${answers.usage}
+${answers.usage}
       
 ${answers.license}
-      
-![Top Language](https://img.shields.io/github/languages/top/${answers.userName}/${answers.repoName}) ![GitHub last commit](https://img.shields.io/github/last-commit/${answers.userName}/${answers.repoName})  ![GitHub Followers](https://img.shields.io/github/followers/${answers.userName}?style=social)
+
+${answers.test}
         
-### ${answers.contributing}
+${answers.contributing}
       
       
-### ${answers.test}
+
+
+![Top Language](https://img.shields.io/github/languages/top/${answers.userName}/${answers.repoName}) ![GitHub last commit](https://img.shields.io/github/last-commit/${answers.userName}/${answers.repoName})  ![GitHub Followers](https://img.shields.io/github/followers/${answers.userName}?style=social)
       
 ${answers.userPic}   
 
